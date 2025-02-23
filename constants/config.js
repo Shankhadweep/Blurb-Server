c61a21cf0769158dbf42c9277d58791b27e52cbc
Shankhadweep Hazra <119057592+Shankhadweep@users.noreply.github.com>
2025-02-23 19:28:23 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,8 +1,9 @@
 const corsOptions = {
   origin: [
+    "*",
+    "https://blurb-frontend-hnog-fschbha21-shankhadweep-hazras-projects.vercel.app/"
     "http://localhost:5173",
     "http://localhost:4173",
-    "*",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
