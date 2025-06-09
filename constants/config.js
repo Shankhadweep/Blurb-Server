344a8c3ebd7589117afa07524fb7465997dacc6b
Shankhadweep Hazra <119057592+Shankhadweep@users.noreply.github.com>
2025-06-09 07:16:05 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -2,6 +2,8 @@ const corsOptions = {
   origin: process.env.CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
+  allowedHeaders:["Content-Type","Authorization"],
+  exposedHeaders:["set-cookie"]
 };
 
 const CHATTU_TOKEN = "chattu-token";
